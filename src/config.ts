2078a559dcac13685311e6f883eb4323de811c5f
SoojinKang0127 <iamhajaml@naver.com>
2026-02-21 23:21:48 +0100

    Remove SQLite artifacts and cleanup legacy source files

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,1 @@
-import path from 'node:path';
-
-export const LEXICON_DB_PATH = path.join(__dirname, '..', 'db', 'lexicon.sqlite');
-export const LEARNING_DB_PATH = path.join(__dirname, '..', 'db', 'learning.sqlite');
-
 export const QUESTION_COUNT = Number(process.argv[2] || 10);
